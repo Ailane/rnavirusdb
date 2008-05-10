fc0729216cc16eee73bf787377d91e812c4ff748
belshaw.robert <belshaw.robert@24ab7048-8e2b-0410-99f9-7ba3584fe236>
2008-05-10 13:37:10 +0000

    minor changes

--- cgi-bin/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// index.php Block directory listings in a way that doesn't depend on the web server
-// being set up to block them or set up to allow .htaccess overrides
- echo "Directory listing denied"
-?>
